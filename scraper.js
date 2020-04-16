80e5b053444c7cda81268089e7ae10bcb6f71e55
Lemons <24715897+Lemons1337@users.noreply.github.com>
2020-04-16 06:20:59 -0400

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -7,7 +7,7 @@ request.get('https://krunker.io', (err, res, body) => {
     var buffer = Buffer.from(str, 'base64').toString('binary');
     var bytes = Buffer.from(buffer);
 
-    var version = bytes.toString().match(/completion(.*?)(\w{5})/s)[1];
+    var version = bytes.toString().match(/completion(.*?)(\w{5})/s)[2];
 
     request.get('https://krunker.io/pkg/krunker.' + version + '.vries', {
         encoding: null
