00830b80a4105f120967f8f775e1ebee6dc56c2d
Lemons <24715897+Lemons1337@users.noreply.github.com>
2020-04-18 16:17:57 -0400

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,26 +1,50 @@
+const beautify = require('js-beautify').js;
 const request = require('request');
 const fs = require('fs');
 
-request.get('https://krunker.io', (err, res, body) => {
-    var str = body.match(/function\((?:\w+(?:,|)){3}\)\{\w+\.exports=\w+\(\d+\)\("(.*?)"\)\}/)[1];
+console.log('[KRUNKER] - Fetching Version...');
 
-    var buffer = Buffer.from(str, 'base64').toString('binary');
-    var bytes = Buffer.from(buffer);
+request.get('https://matchmaker.krunker.io/game-list?hostname=krunker.io', {
+    json: true
+}, (err, res, body) => {
+    var game = body.games[0];
 
-    var version = bytes.toString().match(/__LOADER__mmTokenPromise(.*?)(\w{5})/s)[2];
+    var gameId = game[0];
+    var serverRegion = game[1];
+    var currentPlayers = game[2];
+    var maxPlayers = game[3];
+    var gameInfo = game[4];
+
+    var version = gameInfo.v;
+    var customServer = gameInfo.cs;
+    var gameType = gameInfo.i;
+
+    // Discord - Lemons#0001
+
+    console.log('[KRUNKER] - Got Version (%s)', version);
 
     request.get('https://krunker.io/pkg/krunker.' + version + '.vries', {
         encoding: null
     }, (err, res, buf) => {
         var str = '';
 
+        console.log('[KRUNKER] - Decoding Bytes...');
+
         for (var i = 0; i < buf.byteLength; i++) {
             var byte = buf.readUInt8(i) ^ 0x69;
             str += String.fromCharCode(byte);
         }
 
-        fs.writeFileSync('game.js', str);
+        console.log('[KRUNKER] - Formatting Code...');
 
-        console.log('[KRUNKER] => Version: %s | Saved to game.js', version);
+        str = beautify(str, {
+            indent_size: 4,
+            space_in_empty_paren: true
+        });
+
+        fs.writeFile('game.js', str, (err) => {
+            if (err) throw err;
+            console.log('[KRUNKER] - Saved to game.js');
+        });
     });
 });
