6f53983832b972af9d4c1fb380bbe804d5facfa8
Lemons <24715897+Lemons1337@users.noreply.github.com>
2020-04-21 15:17:59 -0400

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,3 +1,4 @@
+const beautify = require('js-beautify').js;
 const request = require('request');
 const fs = require('fs');
 
@@ -7,13 +8,11 @@ request.get('https://matchmaker.krunker.io/game-list?hostname=krunker.io', {
     json: true
 }, (err, res, body) => {
     var game = body.games[0];
-
     var gameId = game[0];
     var serverRegion = game[1];
     var currentPlayers = game[2];
     var maxPlayers = game[3];
     var gameInfo = game[4];
-
     var version = gameInfo.v;
     var customServer = gameInfo.cs;
     var gameType = gameInfo.i;
@@ -26,9 +25,7 @@ request.get('https://matchmaker.krunker.io/game-list?hostname=krunker.io', {
         encoding: null
     }, (err, res, buf) => {
         var str = '';
-
         console.log('[KRUNKER] - Decoding Bytes...');
-
         for (var i = 0; i < buf.byteLength; i++) {
             var byte = buf.readUInt8(i) ^ 0x69;
             str += String.fromCharCode(byte);
@@ -36,9 +33,19 @@ request.get('https://matchmaker.krunker.io/game-list?hostname=krunker.io', {
 
         console.log('[KRUNKER] - Formatting Code...');
 
+        var code = beautify(str, {
+            indent_size: 4,
+            space_in_empty_paren: true
+        });
+
         fs.writeFile('game.js', str, (err) => {
             if (err) throw err;
-            console.log('[KRUNKER] - Saved to game.js');
+            console.log('[KRUNKER] - Saved original to game.js');
+        });
+
+        fs.writeFile('game_beautify.js', code, (err) => {
+            if (err) throw err;
+            console.log('[KRUNKER] - Saved beautified to game_beautify.js');
         });
     });
 });
