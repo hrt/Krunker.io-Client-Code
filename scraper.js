dd9c58b436a54a390e2b8c25c6d912df4a490a81
Inusha <inusha@outlook.com>
2020-10-30 14:37:18 +0000

    replace beautify library

--- scraper.js
+++ scraper.js
@@ -1,4 +1,4 @@
-const beautify = require('js-beautify').js;
+const nb = require('node-beautify');
 const request = require('request');
 const fs = require('fs');
 
@@ -14,19 +14,29 @@ request.get('https://krunker.io/social.html', (err, res, body) => {
     request.get('https://krunker.io/pkg/krunker.' + version + '.vries', {
         encoding: null
     }, (err, res, buf) => {
-        var str = '';
-        
+        var chars = [];
         console.log('[KRUNKER] - Decoding Bytes...');
         var xor_key = buf.readUInt8(0) ^ '!'.charCodeAt(0);
         console.log('[KRUNKER] - Calculated XOR key (%s)...', xor_key);
         for (var i = 0; i < buf.byteLength; i++) {
             var byte = buf.readUInt8(i) ^ xor_key;
-            str += String.fromCharCode(byte);
+            chars.push(String.fromCharCode(byte));
         }
 
-        fs.writeFile('game.js', str, (err) => {
+        game_js = chars.join('')
+        fs.writeFile('game.js', game_js, (err) => {
             if (err) throw err;
             console.log('[KRUNKER] - Saved original to game.js');
         });
+
+        console.log('[KRUNKER] - Formatting Code...');
+
+        var code = nb.beautifyJs(game_js);
+
+        fs.writeFile('game_beautify.js', code, (err) => {
+            if (err) throw err;
+            console.log('[KRUNKER] - Saved beautified to game_beautify.js');
+        });
+
     });
 });
